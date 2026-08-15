14d399c9dff2ab35de9b74a0161067bca4a151d6
cicero.ladeira <cicero.ladeira@centralit.com.br>
2026-08-15 14:24:37 -0300

    cor verde

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ var app = builder.Build();
 
 app.MapGet("/", () => Results.Content("""
     <html>
-      <body style="background-color: white;">
+      <body style="background-color: green;">
         <h1>Hello World!</h1>
       </body>
     </html>
