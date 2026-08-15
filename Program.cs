4e9d348667116b8bfe5517afd780b882457759ee
cicero.ladeira <cicero.ladeira@centralit.com.br>
2026-08-15 11:06:26 -0300

    adiciona workflow

--- Program.cs
+++ Program.cs
@@ -4,6 +4,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Host.UseWindowsService();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => Results.Content("""
+    <html>
+      <body style="background-color: white;">
+        <h1>Hello World!</h1>
+      </body>
+    </html>
+    """, "text/html"));
 
 app.Run();
